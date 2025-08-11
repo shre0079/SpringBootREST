6c2c2285f44aacbd24fe35878a80c9a3251d34e1
Shreyash <shreyashshree0079@gmail.com>
2025-08-11 20:07:44 +0530

    first commit

---
+++ src/main/java/com/shre/SpringBootRest/service/JobService.java
@@ -0,0 +1,22 @@
+package com.shre.SpringBootRest.service;
+
+import com.shre.JobApp.model.JobPost;
+import com.shre.JobApp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+    @Autowired
+    public JobRepo repo;
+    //method to return all JobPosts
+    public List<JobPost> returnAllJobPosts() {
+        return repo.returnAllJobPosts();
+    }
+    // method to add a jobPost
+    public void addJobPost(JobPost jobPost) {
+        repo.addJobPost(jobPost);
+    }
+}
