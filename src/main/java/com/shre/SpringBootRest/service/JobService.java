1c468d4ee56b144373fc1fb2952e67ce765cd07f
Shreyash <shreyashshree0079@gmail.com>
2025-08-12 11:38:39 +0530

    imports

--- src/main/java/com/shre/SpringBootRest/service/JobService.java
+++ src/main/java/com/shre/SpringBootRest/service/JobService.java
@@ -4,7 +4,6 @@ import com.shre.SpringBootRest.model.JobPost;
 import com.shre.SpringBootRest.repo.JobRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
