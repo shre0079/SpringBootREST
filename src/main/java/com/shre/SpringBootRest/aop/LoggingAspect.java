181feb4c5dda46820425aedfa369a7dd58eb1962
Shreyash <shreyashshree0079@gmail.com>
2025-08-16 19:16:35 +0530

    Logging

--- src/main/java/com/shre/SpringBootRest/aop/LoggingAspect.java
+++ src/main/java/com/shre/SpringBootRest/aop/LoggingAspect.java
@@ -8,7 +8,18 @@ public class LoggingAspect {
 
     private static final Logger log = LoggerFactory.getLogger(LogginAspect.class);
 
-    public void logMethodCall(){
-        log.info("Method called");
+    @After("execution(* com.shre.SpringBootRest.service.JobService.getJob(..)) || execution(* com.shre.SpringBootRest.service.JobService.updateJob(..))")
+    public void logMethodExecuted(JoinPoint joinPoint) {
+        logger.info("Method Executed "+joinPoint.getSignature().getName());
+    }
+
+    @AfterThrowing("execution(* com.shre.SpringBootRest.service.JobService.getJob(..)) || execution(* com.shre.SpringBootRest.service.JobService.updateJob(..))")
+    public void logMethodCrash(JoinPoint joinPoint) {
+        logger.info("Method has some issues "+joinPoint.getSignature().getName());
+    }
+
+    @AfterReturning("execution(* com.shre.SpringBootRest.service.JobService.getJob(..)) || execution(* com.shre.SpringBootRest.service.JobService.updateJob(..))")
+    public void logMethodExecutedSuccessfully(JoinPoint joinPoint) {
+        logger.info("Method executed successfully "+joinPoint.getSignature().getName());
     }
 }
