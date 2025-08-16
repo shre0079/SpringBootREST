d8b37957b341cf87c621dc99dc2cb2326f101864
Shreyash <shreyashshree0079@gmail.com>
2025-08-16 19:17:04 +0530

    logging

--- src/main/java/com/shre/SpringBootRest/aop/LoggingAspect.java
+++ src/main/java/com/shre/SpringBootRest/aop/LoggingAspect.java
@@ -5,6 +5,9 @@ import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+@Component
+@Aspect
 public class LoggingAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
