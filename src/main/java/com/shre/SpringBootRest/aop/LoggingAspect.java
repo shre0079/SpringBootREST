8a3839487ebce81a11bf2fbdd6fd43af0cc57cf5
Shreyash <shreyashshree0079@gmail.com>
2025-08-16 19:16:44 +0530

    logging

--- src/main/java/com/shre/SpringBootRest/aop/LoggingAspect.java
+++ src/main/java/com/shre/SpringBootRest/aop/LoggingAspect.java
@@ -6,7 +6,13 @@ import org.slf4j.LoggerFactory;
 
 public class LoggingAspect {
 
-    private static final Logger log = LoggerFactory.getLogger(LogginAspect.class);
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    //return type, class name, method name, args
+    @Before("execution(* com.shre.SpringBootRest.service.JobService.getJob(..)) || execution(* com.shre.SpringBootRest.service.JobService.updateJob(..))")
+    public void logMethodCall(JoinPoint joinPoint) {
+        logger.info("Method called "+joinPoint.getSignature().getName());
+    }
 
     @After("execution(* com.shre.SpringBootRest.service.JobService.getJob(..)) || execution(* com.shre.SpringBootRest.service.JobService.updateJob(..))")
     public void logMethodExecuted(JoinPoint joinPoint) {
